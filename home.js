e495c3e35b2788fd980b4f4354855536afb1c1e3
Kweryzm <Amessaoudenecontact@gmail.com>
2023-11-07 22:13:36 +0100

    Update the internal structure of tasks

--- home.js
+++ home.js
@@ -21,32 +21,53 @@ document.addEventListener("DOMContentLoaded", function () {
     const listItem = document.createElement("li");
     listItem.classList.add("list-group-item", `list-group-item-${taskCategory}`);
 
-    const nameElement = document.createElement("span");
-    nameElement.classList.add("task-name");
-    nameElement.textContent = taskName;
+    const row1 = document.createElement("div");
+    row1.classList.add("d-flex", "flex-row", "mb-3");
 
-    const categoryElement = document.createElement("span");
-    categoryElement.classList.add("task-category");
-    categoryElement.textContent = taskCategory;
+    const row2 = document.createElement("div");
+    row2.classList.add("d-flex", "flex-row");
 
-    const dateTimeElement = document.createElement("span");
-    dateTimeElement.classList.add("task-date-time");
-    dateTimeElement.textContent = new Date(taskDateTime).toLocaleString();
+    // Create columns for the first row
+    const nameColumn = document.createElement("div");
+    nameColumn.classList.add("col-3", "task-name");
+    nameColumn.textContent = taskName;
+
+    const typeColumn = document.createElement("div");
+    typeColumn.classList.add("col-3", "task-category");
+    typeColumn.textContent = taskCategory;
+
+    const dateHourColumn = document.createElement("div");
+    dateHourColumn.classList.add("col-3", "task-date-time");
+    dateHourColumn.textContent = new Date(taskDateTime).toLocaleString();
+
+    const statusColumn = document.createElement("div");
+    statusColumn.classList.add("col-3", "task-status");
 
     const taskStatusSelect = document.createElement("select");
-    taskStatusSelect.classList.add("form-select", "task-status");
+    taskStatusSelect.classList.add("form-select");
     taskStatusSelect.innerHTML = `
         <option value="done" style="color: green;background-color:#7FFF89">Done</option>
         <option value="in-progress" style="color: yellow;background-color:#95A100">In Progress</option>
         <option value="not-yet" style="color: red;background-color:#810202">Not Yet</option>
     `;
 
+    statusColumn.appendChild(taskStatusSelect);
+
+    // Create columns for the second row (Edit and Delete buttons)
+    const editButtonColumn = document.createElement("div");
+    editButtonColumn.classList.add("col-6");
+
     const editButton = document.createElement("button");
-    editButton.classList.add("btn", "btn-light", "btn-sm", "mx-2", "edit-button");
+    editButton.classList.add("btn", "btn-light", "btn-sm", "edit-button");
     editButton.innerText = "Edit";
     editButton.addEventListener("click", function () {
         editTask(listItem);
     });
+
+    editButtonColumn.appendChild(editButton);
+
+    const deleteButtonColumn = document.createElement("div");
+    deleteButtonColumn.classList.add("col-6");
 
     const deleteButton = document.createElement("button");
     deleteButton.classList.add("btn", "btn-danger", "btn-sm");
@@ -55,12 +76,19 @@ document.addEventListener("DOMContentLoaded", function () {
         deleteTask(listItem);
     });
 
-    listItem.appendChild(nameElement);
-    listItem.appendChild(categoryElement);
-    listItem.appendChild(dateTimeElement);
-    listItem.appendChild(taskStatusSelect);
-    listItem.appendChild(editButton);
-    listItem.appendChild(deleteButton);
+    deleteButtonColumn.appendChild(deleteButton);
+
+    // Append columns to the rows
+    row1.appendChild(nameColumn);
+    row1.appendChild(typeColumn);
+    row1.appendChild(dateHourColumn);
+    row1.appendChild(statusColumn);
+
+    row2.appendChild(editButtonColumn);
+    row2.appendChild(deleteButtonColumn);
+
+    listItem.appendChild(row1);
+    listItem.appendChild(row2);
 
     // Set the initial status to "Not Yet"
     taskStatusSelect.value = "not-yet";
@@ -68,33 +96,34 @@ document.addEventListener("DOMContentLoaded", function () {
     taskList.appendChild(listItem);
 }
 
-function editTask(listItem) {
-    const nameElement = listItem.querySelector(".task-name");
-    const editButton = listItem.querySelector(".edit-button");
-    if (editButton.innerText === "Edit") {
-        const inputElement = document.createElement("input");
-        inputElement.classList.add("form-control", "task-name-input");
-        inputElement.value = nameElement.textContent;
-        nameElement.style.display = "none";
-        listItem.insertBefore(inputElement, nameElement);
-        editButton.innerText = "OK";
 
-        // Change button classes to make it green
-        editButton.classList.remove("btn-light");
-        editButton.classList.add("btn-success");
-        inputElement.focus();
-    } else if (editButton.innerText === "OK") {
-        const inputElement = listItem.querySelector(".task-name-input");
-        nameElement.textContent = inputElement.value;
-        nameElement.style.display = "inline";
-        inputElement.remove();
-        editButton.innerText = "Edit";
+    function editTask(listItem) {
+        const nameElement = listItem.querySelector(".task-name");
+        const editButton = listItem.querySelector(".edit-button");
+        if (editButton.innerText === "Edit") {
+            const inputElement = document.createElement("input");
+            inputElement.classList.add("form-control", "task-name-input");
+            inputElement.value = nameElement.textContent;
+            nameElement.style.display = "none";
+            listItem.querySelector(".task-left").insertBefore(inputElement, nameElement);
+            editButton.innerText = "OK";
 
-        // Change button classes to make it light
-        editButton.classList.remove("btn-success");
-        editButton.classList.add("btn-light");
+            // Change button classes to make it green
+            editButton.classList.remove("btn-light");
+            editButton.classList.add("btn-success");
+            inputElement.focus();
+        } else if (editButton.innerText === "OK") {
+            const inputElement = listItem.querySelector(".task-name-input");
+            nameElement.textContent = inputElement.value;
+            nameElement.style.display = "inline";
+            inputElement.remove();
+            editButton.innerText = "Edit";
+
+            // Change button classes to make it light
+            editButton.classList.remove("btn-success");
+            editButton.classList.add("btn-light");
+        }
     }
-}
 
     function deleteTask(listItem) {
         if (confirm("Are you sure you want to delete this task?")) {
